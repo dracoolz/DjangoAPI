9fab3c435a181f5271c8e35bdaae3a7006d645d2
Kid キッド <2200339@ecc.ac.jp>
2022-10-20 13:18:23 +0900

    debug finished

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path 
+from users import views 
+ 
+urlpatterns = [ 
+    re_path(r'^api/user$', views.user_list),
+    re_path(r'^api/users/(?P<pk>[0-9]+)$', views.user_detail),
+    re_path(r'^api/users/published$', views.user_list_published)
+]
---
+++ users/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+from users.models import Users
+from users.serializers import UserSerializer
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def user_list(request):
+    if request.method == 'GET':
+        users = Users.objects.all()
+        
+        title = request.query_params.get('title', None)
+        if title is not None:
+            users = Users.filter(title__icontains=title)
+        
+        user_serializer = UserSerializer(users, many=True)
+        return JsonResponse(user_serializer.data, safe=False)
+        # 'safe=False' for objects serialization
+ 
+    elif request.method == 'POST':
+        user_data = JSONParser().parse(request)
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse(user_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        count = Users.objects.all().delete()
+        return JsonResponse({'message': '{} Users were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+ 
+ 
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try: 
+        user = Users.objects.get(pk=pk) 
+    except Users.DoesNotExist: 
+        return JsonResponse({'message': 'The user does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+ 
+    if request.method == 'GET': 
+        user_serializer = UserSerializer(user) 
+        return JsonResponse(user_serializer.data) 
+ 
+    elif request.method == 'PUT': 
+        user_data = JSONParser().parse(request) 
+        user_serializer = UserSerializer(user, data=user_data) 
+        if user_serializer.is_valid(): 
+            user_serializer.save() 
+            return JsonResponse(user_serializer.data) 
+        return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+ 
+    elif request.method == 'DELETE': 
+        user.delete() 
+        return JsonResponse({'message': 'User was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+    
+        
+@api_view(['GET'])
+def user_list_published(request):
+    user = Users.objects.filter(published=True)
+        
+    if request.method == 'GET': 
+        user_serializer = UserSerializer(user, many=True)
+        return JsonResponse(user_serializer.data, safe=False)
+    
