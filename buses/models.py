870d20333ee71e69150f9fbfa87f44c8ef34abc0
Kid キッド <2200339@ecc.ac.jp>
2022-11-08 11:56:56 +0900

    fix model data type for buses

--- buses/models.py
+++ buses/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Buses(models.Model):
     name = models.CharField(max_length=70, blank=False, default='')
-    group_id = models.CharField(max_length=70, blank=False, default='')
+    group_id = models.IntegerField(blank=False, default='')
     created = models.DateTimeField(auto_now_add=True)
