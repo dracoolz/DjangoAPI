5403a8718a08986b57e3aff4b119d2d2967ede38
Kid キッド <2200339@ecc.ac.jp>
2022-11-08 09:26:56 +0900

    fix url pathing

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from users import views 
  
 urlpatterns = [ 
-    re_path(r'^api/users$', views.user_list),
-    re_path(r'^api/users/(?P<pk>[0-9]+)$', views.user_detail),
-    re_path(r'^api/users/published$', views.user_list_published)
+    re_path(r'users$', views.user_list),
+    re_path(r'users/(?P<pk>[0-9]+)$', views.user_detail),
+    re_path(r'users/published$', views.user_list_published)
 ]
