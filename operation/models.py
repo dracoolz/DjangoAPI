f2a520f7fc8bf218db1dca92621e15d6d401d17a
Kid キッド <2200339@ecc.ac.jp>
2022-11-22 10:02:56 +0900

    fixed relation and model data

--- operation/models.py
+++ operation/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+#Relation
+from buses.models import Buses
 class Operation(models.Model):
     start = models.BooleanField(blank=False, default='')
     end = models.BooleanField(blank=False, default='')
-    bus_id = models.IntegerField(blank=False, default='')
+    bus_id = models.ForeignKey(Buses, on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
