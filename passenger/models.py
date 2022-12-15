7ad45fc14112197c4188bebe611169e6f17c5420
yumekiti <yumekiti120440@gmail.com>
2022-12-15 18:12:33 +0900

    fix: passenger status

--- passenger/models.py
+++ passenger/models.py
@@ -5,5 +5,6 @@ from users.models import Users
 class Passenger(models.Model):
     operation_id = models.ForeignKey(Operation, on_delete=models.CASCADE, unique=True, blank=True, null=True, default=None)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE, unique=True, blank=True, null=True, default=None)
-    status = models.BooleanField(blank=False, unique=True)
+    # status = models.BooleanField(blank=False, unique=True)
+    status = models.BooleanField(blank=False)
     created = models.DateTimeField(auto_now_add=True)
