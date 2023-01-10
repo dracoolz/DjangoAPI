18ed7c10790561f8bc5b6e822ab96c4e229cf3ae
Kid キッド <2200339@ecc.ac.jp>
2023-01-10 10:07:11 +0900

    add buses_id, delete group_id

--- buses/models.py
+++ buses/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-#Relation
-from groups.models import Groups
 class Buses(models.Model):
     name = models.CharField(max_length=70, blank=False, unique=True)
-    group_id = models.ForeignKey(Groups, on_delete=models.CASCADE, blank=True, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True)
--- groups/models.py
+++ groups/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from buses.models import Buses
 class Groups(models.Model):
     name = models.CharField(max_length=70, blank=False, unique=True)
     address = models.CharField(max_length=200,blank=False, null=True)
+    buses_id = models.ForeignKey(Buses, on_delete=models.CASCADE, blank=True, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True)
