eae9615141461dafc48318231c2f004910e53e5b
Kid キッド <2200339@ecc.ac.jp>
2023-01-12 14:45:27 +0900

    remove operation_id & user_id's unique from passenger

--- passenger/models.py
+++ passenger/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from operation.models import Operation
 from users.models import Users
 class Passenger(models.Model):
-    operation_id = models.ForeignKey(Operation, on_delete=models.CASCADE, unique=True, blank=True, null=True, default=None)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, unique=True, blank=True, null=True, default=None)
+    operation_id = models.ForeignKey(Operation, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True, default=None)
     status = models.BooleanField(blank=False, unique=False)
     created = models.DateTimeField(auto_now_add=True)
